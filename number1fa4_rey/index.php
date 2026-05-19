133f965b444b9eeee56e97a069196f2939102c06
jwuuwiee <jusirey@gmail.com>
2026-05-19 11:14:37 +0800

    Create index.php

---
+++ number1fa4_rey/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Story Collection</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php include("navbar.php"); ?>
+
+<div class="container">
+
+    <div class="card">
+        <img src="images/book1.jpg">
+        <h2>Book 1 - The Sound of Falling in Love</h2>
+        <a href="book1.php">Read Story</a>
+    </div>
+
+    <div class="card">
+        <img src="images/book2.jpg">
+        <h2>Book 2 - How to Fall for Your Rival</h2>
+        <a href="book2.php">Read Story</a>
+    </div>
+
+    <div class="card">
+        <img src="images/book3.jpg">
+        <h2>Book 3 - When the Noise Fades</h2>
+        <a href="book3.php">Read Story</a>
+    </div>
+
+    <div class="card">
+        <img src="images/book4.jpg">
+        <h2>Book 4 - Oops, We Caught Feelings</h2>
+        <a href="book4.php">Read Story</a>
+    </div>
+
+    <div class="card">
+        <img src="images/book5.jpg">
+        <h2>Book 5 - We Should've Stayed Broken Up</h2>
+        <a href="book5.php">Read Story</a>
+    </div>
+
+</div>
+
+<?php include("footer.php"); ?>
+
+</body>
+</html>
