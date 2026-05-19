1402823f8d0d3d6da17a6d11959fabf1750b7f1d
jwuuwiee <jusirey@gmail.com>
2026-05-19 12:55:45 +0800

    Create friend4.php

---
+++ number1fa4_rey/friend4.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Niko</title>
+    <style>
+        body { font-family: Arial; background:#f7f7fb; margin:0; }
+        .profile {
+            max-width:700px; margin:50px auto; background:white;
+            padding:20px; border-radius:15px; text-align:center;
+        }
+        img { width:100%; height:350px; object-fit:cover; border-radius:15px; }
+    </style>
+</head>
+<body>
+
+<?php include("navbar.php"); ?>
+
+<div class="profile">
+    <img src="images/friend4.jpg">
+    <h1>Niko</h1>
+    <p>
+       Energetic and fun to be with. Gives helpful advice. My cousin but also part of my friend group since elementary. Taking BS Speech Language Pathology in University of Santo Tomas
+    </p>
+    <a href="index.php">← Back</a>
+</div>
+
+<div class="footer">
+    <p>Created by Juliana Rey</p>
+</div>
+
+</body>
+</html>
